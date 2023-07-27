2e975a27bcfa6bbb43d81d0178fb8871effbf453
Azhar <azhar.subair.2@gmail.com>
2023-07-27 14:29:01 +0530

    Initial commit

---
+++ machinetest/src/app/components/display/display.component.ts
@@ -0,0 +1,19 @@
+import { Component } from '@angular/core';
+import { Data } from '../../services/data';
+
+@Component({
+  selector: 'app-display',
+  templateUrl: './display.component.html',
+  styleUrls: ['./display.component.css']
+})
+export class DisplayComponent {
+  data: any[] = Data;
+  selection:any=[];
+  Checklist=['COMP_M_VIEW','SOP_A_VIEW','TEST_M_ADD','QS0P_V_VIEW']
+
+  select(rec:any){
+    this.selection = rec;
+
+  }
+
+}
